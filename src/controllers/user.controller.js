9c973e0c0c8b11117e70c0b876eaee81ba925754
Wasiq-dev-code <study123steel@gmail.com>
2025-05-05 01:53:44 +0500

    Add user channel profile and history retrieval functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/User.model.js";
 import { deleteOnCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAcessAndRefreshAtoken = async (userId) => {
   try {
@@ -405,6 +406,123 @@ const changeCoverImg = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, { user }, "avatar changed successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username.trim()) {
+    throw new ApiError(400, "username should be pass through URL");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id", //must read about
+        foreignField: "channel",
+        as: "Totalsubscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "SubscribeTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$Totalsubscribers",
+        },
+        subscribeToCount: {
+          $size: "$SubscribeTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$Totalsubscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        username: 1,
+        email: 1,
+        fullname: 1,
+        coverImg: 1,
+        avatar: 1,
+        subscribersCount: 1,
+        subscribeToCount: 1,
+        isSubscribed: 1,
+      },
+    },
+  ]);
+
+  if (!channel.length) {
+    throw new ApiError(400, "channel is not provided");
+  }
+
+  return res
+    .status()
+    .json(new ApiResponse(200, channel[0], "channel is provided"));
+});
+
+const getUserHistory = asyncHandler(async (req, res) => {
+  const userHistory = User.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userHistory.watchHistory, "userWatchHistory"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -415,4 +533,5 @@ export {
   updateFields,
   changeAvatar,
   changeCoverImg,
+  getUserChannelProfile,
 };
