42e6902eba4b8161f2183ed6cb432caad4c7be5f
Wasiq-dev-code <study123steel@gmail.com>
2025-05-09 02:34:49 +0500

    Refactor video upload route to include JWT verification middleware

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { upload } from "../utils/cloudinary.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { videoUploader } from "../controllers/Video.controller";
+import { JWTVerify } from "../middlewares/auth.middleware.js";
 const videoRouter = Router();
 
 videoRouter.route("/video/upload-video").post(
+  JWTVerify,
   upload.fields([
     {
       name: "videoFile",
