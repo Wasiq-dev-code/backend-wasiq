08a34a6c91a7246872cc73466b0c9ed614eba90f
Wasiq-dev-code <study123steel@gmail.com>
2025-05-11 14:37:46 +0500

    Refactor video routes to separate public and secure routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { videoUploader } from "../controllers/Video.controller";
+import { getAllVideos, videoUploader } from "../controllers/Video.controller";
 import { JWTVerify } from "../middlewares/auth.middleware.js";
 const videoRouter = Router();
 
-videoRouter.route("/video/upload-video").post(
+/// Public Routes
+videoRouter.route("/").get(getAllVideos);
+
+/// Secure Routes
+videoRouter.route("/video/upload").post(
   JWTVerify,
   upload.fields([
     {
