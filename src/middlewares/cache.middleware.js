d2db50a98bc3e0e21e13a72bd9ee3ae8a3836065
Wasiq-dev-code <study123steel@gmail.com>
2025-05-17 05:32:10 +0500

    Add cache middleware for Redis to optimize response handling

---
+++ src/middlewares/cache.middleware.js
@@ -0,0 +1,24 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import client from "../config/redis";
+const cacheFromRedis = (prefix, duration) => async (req, res, next) => {
+  const key = `${prefix}:${req.originalUrl}`;
+  try {
+    const cached = await client.get(key);
+    if (cached) {
+      return res.json(JSON.parse(cached));
+    }
+
+    const originalResponse = res.json;
+
+    res.json = async function (body) {
+      await client.setEx(key, duration, JSON.stringify(body)).catch((err) => {
+        console.error("Error while set data", err);
+      });
+      return originalResponse.call(this, body);
+    };
+    next();
+  } catch (error) {
+    console.error("Occuring while running cacheMiddleware");
+    next();
+  }
+};
