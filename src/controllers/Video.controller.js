2682da6823f61cd9696f9fb8ddf727e2e7d1df59
Wasiq-dev-code <study123steel@gmail.com>
2025-05-18 12:44:35 +0500

    changing file name

--- src/controllers/Video.controller.js
+++ src/controllers/Video.controller.js
@@ -1,11 +1,11 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { updateVideo } from "../services/video/updateVideo.js";
-import { deleteVideo } from "../services/video/deleteVideo.js";
-import { getVideoById } from "../services/video/getVideoById.js";
-import { getAllVideos } from "../services/video/getAllVideos.js";
-import { videoUploader } from "../services/video/videoUploader.js";
+import { updateVideo } from "../services/video/updateVideo.service.js";
+import { deleteVideo } from "../services/video/deleteVideo.service.js";
+import { getVideoById } from "../services/video/getVideoById.service.js";
+import { getAllVideos } from "../services/video/getAllVideos.service.js";
+import { videoUploader } from "../services/video/videoUploader.service.js";
 
 const videoUploaderController = asyncHandler(async (req, res) => {
   try {
