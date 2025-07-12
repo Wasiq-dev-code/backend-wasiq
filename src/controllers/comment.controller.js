ce81239f33de03ffb9867804b53c478bc21cccfe
Wasiq-dev-code <study123steel@gmail.com>
2025-07-13 01:01:58 +0500

    Add getCommentByVideo and getReplyToComment services; implement corresponding controllers for fetching comments and replies

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,7 @@
 import { addComment } from "../services/comment/addComment.service.js";
 import { deleteComment } from "../services/comment/deleteComment.service.js";
+import { getCommentByVideo } from "../services/comment/getCommentByVideo.service.js";
+import { getReplyToComment } from "../services/comment/getReplyToComment.service.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -46,4 +48,43 @@ const deleteCommentController = asyncHandler(async (req, res) => {
   }
 });
 
-export { addCommentController, deleteCommentController };
+const getCommentByVideoController = asyncHandler(async (req, res) => {
+  try {
+    const videoId = req.params?.videoId;
+
+    const comments = await getCommentByVideo({
+      videoId,
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, comments, "all comments of videos"));
+  } catch (error) {
+    console.error(error.message || "server is not responding");
+    throw new ApiError(500, "server is not responding");
+  }
+});
+
+const getReplyToCommentController = asyncHandler(async (req, res) => {
+  try {
+    const commentId = req.params?.commentId;
+
+    const replies = await getReplyToComment({
+      commentId,
+    });
+
+    return res
+      .status(201)
+      .json(new ApiResponse(201, replies, "all replies of comments"));
+  } catch (error) {
+    console.error(error.message || "server is not responding");
+    throw new ApiError(500, "server is not responding");
+  }
+});
+
+export {
+  addCommentController,
+  deleteCommentController,
+  getCommentByVideoController,
+  getReplyToCommentController,
+};
