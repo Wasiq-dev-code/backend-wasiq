5e6929ce16d7f0fa3aea237fe62630fb94cd2258
Wasiq-dev-code <study123steel@gmail.com>
2025-10-06 07:37:07 +0500

    feat: Add unit tests for getSortedQuery function and handle null/undefined cases

---
+++ src/tests/unit/generateCacheKey.test.js
@@ -0,0 +1,48 @@
+import { getSortedQuery } from "../../utils/helpers/getSortedQuery.js";
+import { describe, expect, test } from "@jest/globals";
+
+describe("GenerateCacheKey -- Unit Test", () => {
+  test("Expecting to sort an object to alphabetical string", () => {
+    const query = {
+      age: 10,
+      name: "wim",
+    };
+
+    const result = getSortedQuery(query);
+
+    expect(result).toBe("age=10&name=wim");
+  });
+
+  test("should handle single key object", () => {
+    const query = { z: 99 };
+    const result = getSortedQuery(query);
+
+    expect(result).toBe("z=99");
+  });
+
+  test("should return empty string for empty object", () => {
+    const query = {};
+    const result = getSortedQuery(query);
+
+    expect(result).toBe("");
+  });
+
+  test("should handle numeric values correctly", () => {
+    const query = { b: 20, a: 10 };
+    const result = getSortedQuery(query);
+
+    expect(result).toBe("a=10&b=20");
+  });
+
+  test("should handle the object which has multiple type of data", () => {
+    const query = { b: 20, a: "madd" };
+    const result = getSortedQuery(query);
+
+    expect(result).toBe("a=madd&b=20");
+  });
+
+  test("should return empty string if query is null or undefined", () => {
+    expect(getSortedQuery(undefined)).toBe("");
+    expect(getSortedQuery(null)).toBe("");
+  });
+});
--- src/utils/helpers/getSortedQuery.js
+++ src/utils/helpers/getSortedQuery.js
@@ -1,4 +1,5 @@
 export const getSortedQuery = (query) => {
+  if (!query || typeof query !== "object") return "";
   return Object.keys(query)
     .sort()
     .map((key) => `${key}=${query[key]}`)
