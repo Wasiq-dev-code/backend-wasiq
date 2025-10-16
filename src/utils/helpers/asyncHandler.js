1f92f15ebe30376491297d33f8af6108e4bfd770
Wasiq-dev-code <study123steel@gmail.com>
2025-10-16 07:50:48 +0500

    docs: Update documentation for asyncHandler function to improve clarity and usage example

--- src/utils/helpers/asyncHandler.js
+++ src/utils/helpers/asyncHandler.js
@@ -1,3 +1,19 @@
+/**
+ * @function asyncHandler
+ * @description Utility to wrap async function in asynchandler to provide better error monitoring specially for controller and services. No need to Try/Catch after wrapping asynchandler.
+ *
+ * @param {Function} fn - Async function with signature. (req, res, next) => {}
+ * @returns {Function} - Express-Compatible Middleware. (req, res, next) => {}
+ *
+ * // Usage example:
+ *
+ * import {asycnHandler} from "/asycnHandler.js"
+ *
+ * router.get("/users", asyncHandler(async(req, res, next) => {
+ * // Existing code
+ * }))
+ */
+
 const asyncHandler = (fn) => (req, res, next) => {
   return Promise.resolve(fn(req, res, next)).catch((err) => next(err));
 };
